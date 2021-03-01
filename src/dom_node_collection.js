5456593a58d0c0029583de04db02ae51f9413561
Brad Trick <btrick@gmail.com>
2021-03-01 17:51:11 -0500

    Add functions through find()

--- src/dom_node_collection.js
+++ src/dom_node_collection.js
@@ -1,7 +1,88 @@
 class DOMNodeCollection {
     constructor(arr) {
-        this.arr = arr;
+        this.nodes = arr;
+    }
+
+    html(string) {
+        if (string !== undefined) {
+            this.nodes.forEach( (el) => el.innerHTML = string);
+            return this;
+        }
+        else
+            return this.nodes[0].innerHTML;
+    }
+
+    empty() {
+        this.nodes.forEach((el) => el.innerHTML = "");
+    }
+
+    append (...content) {
+        this.nodes.forEach (node => {
+            content.forEach((el) => {
+                if (el instanceof HTMLElement){
+                    node.insertAdjacentHTML('beforeend',el.outerHTML);
+                } else if (el instanceof DOMNodeCollection){
+                    el.nodes.forEach(function(el2){
+                        node.insertAdjacentHTML('beforeend', el2.outerHTML);
+                    })
+                }
+                else {
+                    node.insertAdjacentHTML('beforeend', el);
+                }
+            });
+        });
+    }
+
+    addClass (...classes) {
+        this.nodes.forEach(function(node){
+                node.classList.add(...classes);   
+        })
+    }
+
+    removeClass (...classes) {
+        this.nodes.forEach(function(node){
+                node.classList.remove(...classes);   
+        })
+    }
+
+    attr (name, value){
+        if (value === undefined) {
+            return this.nodes.map(function(el){
+                el.getAttribute(name);
+            })
+        }
+        this.nodes.forEach(function(node){
+            node.setAttribute(name, value);
+        })
+    }
+
+    children (){
+        let arr = [];
+        this.nodes.forEach(function(node){
+            arr = arr.concat(Array.from(node.children));
+        })
+        return new DOMNodeCollection(arr);
+    }
+
+    parent() {
+        let arr = [];
+        this.nodes.forEach(function (node) {
+            if (node.parentNode && !arr.includes(node.parentNode))
+                arr.push(node.parentNode);
+        })
+        return new DOMNodeCollection(arr);
+    }
+
+    //Still to do: Need to only push unique elements, as in parent
+    find(selectors) {
+        let arr = [];
+        this.nodes.forEach(function (node) {
+            arr = arr.concat(Array.from(node.querySelectorAll(selectors)));
+        })
+        return new DOMNodeCollection(arr);
     }
 }
+
+
 
 module.exports = DOMNodeCollection;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
 const DOMNodeCollection = require("./dom_node_collection.js");
 
 window.$l = function(arg) {
-    let obj = Array.from(document.querySelectorAll(arg));
-    let collection = new DOMNodeCollection(obj);
-    return collection;
+    if (arg instanceof HTMLElement){
+        return new DOMNodeCollection([arg]);
+    }
+    else {
+        let obj = Array.from(document.querySelectorAll(arg));
+        return new DOMNodeCollection(obj);
+    }
 };
